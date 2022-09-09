ca3cdeb7ce3eccd99c80a7ceaa357d6b15f6e7fe
PUGE <mail@puge.org>
2022-09-09 17:10:15 +0800

    save

--- dist/static/js/owo.core.js
+++ dist/static/js/owo.core.js
@@ -1,4 +1,4 @@
-// Wed Sep 07 2022 17:05:45 GMT+0800 (中国标准时间)
+// Fri Sep 09 2022 16:49:35 GMT+0800 (中国标准时间)
 var owo = {tool: {},state: {},event: {}};
 /* 方法合集 */
 var _owo = {
@@ -228,6 +228,14 @@ _owo.addEvent = function (tempDom, moudleScript) {
 
 
 
+window.addEventListener("popstate", function(e) {
+  // 修复有时候hash和view会同时变化无法刷新的问题
+  setTimeout(function () {
+    if (_owo.getarg(document.URL) !== owo.activePage) _owo.hashchange()
+    _owo.getViewChange()
+  }, 200);
+}, false);
+
 
 _owo.cutString = function (original, before, after, index) {
   index = index || 0
@@ -272,6 +280,18 @@ _owo.cutStringArray = function (original, before, after, index, inline) {
 }
 
 
+
+// 获取URL中的参数
+_owo.getQueryVariable = function () {
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  var temp = {}
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split("=");
+    temp[pair[0]] = pair[1];
+  }
+  return temp;
+}
 
 
 // 页面切换
@@ -442,6 +462,30 @@ function owoPageInit () {
   }
   recursion(this)
   
+  // 判断页面中是否有路由
+  if (this.view) {
+    if (!this.view._isCreated) {
+      this.view._isCreated = true
+      temp = []
+      for (var viewName in this.view) {
+        // 跳过系统添加的字段
+        if (viewName[0] == '_') continue
+        var routeList = this.view[viewName]
+        this.view[viewName] = new View(routeList, viewName, this['$el'], this)
+        temp.push(this.view[viewName])
+      }
+      _owo.getViewChange()
+      this.view._list = temp
+    } else {
+      // 运行每个激活路由的show方法
+      for (var index in this.view._list) {
+        var routeItem = this.view._list[index]
+        var pageObj = routeItem[routeItem._activeName]
+        if (pageObj.show && pageObj.$el) routeItem[routeItem._activeName].show()
+      }
+    } 
+  }
+  
   
 }
 
@@ -517,6 +561,151 @@ _owo.addHTMLElementFun = function (name, func) {
 _owo.addHTMLElementFun('query', function(str) {
   return this.querySelector(str)
 })
+
+
+// 特殊类型
+function View(routeList, viewName, entryDom, pageScript) {
+  this._list = []
+  this._viewName = viewName
+  this.$el = entryDom.querySelector('[view="' + viewName +'"]')
+  for (var routeInd = 0; routeInd < routeList.length; routeInd++) {
+    var routeItem = routeList[routeInd]
+    this._list[routeInd] = routeItem
+    this._list[routeInd]._index = routeInd
+    this._list[routeInd].$el = entryDom.querySelector('[view="' + viewName +'"] [route="' + routeItem._name +'"]')
+    // 默认隐藏route
+    this._list[routeInd].$el.setAttribute('route-active', 'false')
+    // 错误处理
+    if (!this._list[routeInd].$el) {
+      console.error('找不到视窗 ' + viewName + ' 中的路由: ' + routeItem._name)
+      break
+    }
+    this._list[routeInd] = new Page(this._list[routeInd], pageScript)
+    this._list[routeInd].$el.setAttribute('route-ind', routeInd)
+    this[routeItem._name] = this._list[routeInd]
+  }
+}
+
+owo.state.routeBusy = false
+owo.state.viewBusy = false
+View.prototype.showIndex = function (ind) {
+  var viewName = this._viewName
+  if (owo.state.viewBusy) return
+  owo.state.viewBusy = true
+  // 防止来回快速切换页面出问题
+  if (owo.state[viewName + '_changeing']) return
+  owo.state[viewName + '_changeing'] = true
+  this._activeIndex = this._activeIndex
+  var oldRoute = this._list[this._activeIndex]
+  // 如果新旧路由和旧路由是一样的那么不做处理
+  if (this._activeIndex == ind) {
+    oldRoute.$el.setAttribute('route-active', 'true')
+    owo.state[viewName + '_changeing'] = false
+    owo.state.viewBusy = false
+    return
+  }
+  var newRoute = this._list[ind]
+  if (!newRoute) {console.error('导航到不存在的页面: ' + ind);return;}
+  this["_activeName"] = newRoute._name
+  this["_activeIndex"] = ind
+  newRoute.owoPageInit()
+  newRoute.handleEvent()
+  if (oldRoute) {
+    function clearRoute () {
+      owo.state[viewName + '_changeing'] = false
+      oldRoute.$el.setAttribute('route-active', 'false')
+      owo.state.viewBusy = false
+    }
+    var animationValue = owo.state._animation || owo.globalAni
+    if (animationValue) {
+      
+      if (newRoute._index > oldRoute._index) _owo.animation(oldRoute.$el, newRoute.$el, animationValue.in, animationValue.out)
+      else _owo.animation(oldRoute.$el, newRoute.$el, animationValue.backIn, animationValue.backOut)
+      // 加个延时隐藏不然直接隐藏动画效果不好
+      setTimeout(clearRoute, 800);
+    } else {
+      _owo.animation(oldRoute.$el, newRoute.$el)
+      clearRoute()
+    }
+    
+  } else {
+    owo.state[viewName + '_changeing'] = false
+  }
+  newRoute.$el.setAttribute('route-active', 'true')
+  owo.onViewChange()
+  owo.state.viewBusy = false
+}
+
+View.prototype.showName = function (name) {
+  var viewName = this._viewName
+  if (owo.state.viewBusy) return
+  owo.state.viewBusy = true
+  // 防止来回快速切换页面出问题
+  if (owo.state[viewName + '_changeing']) return
+  owo.state[viewName + '_changeing'] = true
+
+  var oldRoute = this[this._activeName]
+  var newRoute = this[name]
+  if (!newRoute) {console.error('导航到不存在的页面: ' + name);return;}
+  // 如果新旧路由和旧路由是一样的那么不做处理
+  if (this._activeName == name) {
+    oldRoute.$el.setAttribute('route-active', 'true')
+    owo.state[viewName + '_changeing'] = false
+    owo.state.viewBusy = false
+    return
+  }
+  // 根据index
+  this["_activeName"] = newRoute._name
+  this["_activeIndex"] = newRoute._index
+  // 如果没有旧路由，那么直接显示新路由就行
+  newRoute.owoPageInit()
+  newRoute.handleEvent()
+  if (oldRoute) {
+    function clearRoute () {
+      owo.state[viewName + '_changeing'] = false
+      oldRoute.$el.setAttribute('route-active', 'false')
+      owo.state.viewBusy = false
+    }
+    var animationValue = owo.state._animation || owo.globalAni
+    if (animationValue) {
+      if (newRoute._index > oldRoute._index) _owo.animation(oldRoute.$el, newRoute.$el, animationValue.in, animationValue.out)
+      else _owo.animation(oldRoute.$el, newRoute.$el, animationValue.backIn, animationValue.backOut)
+      // 加个延时隐藏不然直接隐藏动画效果不好
+      setTimeout(clearRoute, 800);
+    } else {
+      _owo.animation(oldRoute.$el, newRoute.$el)
+      clearRoute()
+    }
+    
+  } else {
+    owo.state[viewName + '_changeing'] = false
+    owo.state.viewBusy = false
+  }
+  newRoute.$el.setAttribute('route-active', 'true')
+  owo.onViewChange()
+}
+View.prototype.owoPageInit = owoPageInit
+View.prototype.handleEvent = handleEvent
+
+owo.onViewChange = function () {}
+
+_owo.getViewChange = function () {
+  var activeScript = owo.script[owo.activePage]
+  // 路由列表
+  var viewList = activeScript.$el.querySelectorAll('[view]')
+  // 获取url参数
+  owo.state.urlVariable = _owo.getQueryVariable()
+  for (var index = 0; index < viewList.length; index++) {
+    var viewItem = viewList[index];
+    var viewName = viewItem.getAttribute('view')
+    var viewValue = owo.state.urlVariable['view-' + viewName]
+    if (viewValue) {
+      activeScript.view[viewName].showName(viewValue)
+    } else {
+      activeScript.view[viewName].showIndex(0)
+    }
+  }
+}
 
 
 
@@ -706,11 +895,6 @@ _owo.showPage = function() {
   // 取出URL地址判断当前所在页面
   var pageArg = _owo.getarg(window.location.hash)
   
-  if (pageArg !== null) {
-    window.location.href = ''
-    return
-  }
-  
   
 
   // 从配置项中取出程序入口
@@ -848,4 +1032,21 @@ function switchPage (oldUrlParam, newUrlParam) {
 if (window.onhashchange) {window.onhashchange = _owo.hashchange;} else {window.onpopstate = _owo.hashchange;}
 // 执行页面加载完毕方法
 _owo.ready(_owo.showPage)
+
+
+// 这是用于代码调试的自动刷新代码，他不应该出现在正式上线版本!
+if ("WebSocket" in window) {
+  // 打开一个 web socket
+  if (!window._owo.ws) window._owo.ws = new WebSocket("ws://" + window.location.host)
+  window._owo.ws.onmessage = function (evt) { 
+    if (evt.data == 'reload') {
+      location.reload()
+    }
+  }
+  window._owo.ws.onclose = function() { 
+    console.info('与服务器断开连接')
+  }
+} else {
+  console.error('浏览器不支持WebSocket')
+}
 
